239df907cb1d34aef70b87f42b24671b7bf9e1c0
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2020-09-10 10:40:08 -0300

    ...

--- sql/APP.sql
+++ sql/APP.sql
@@ -10,7 +10,7 @@ DELETE FROM cfg_app WHERE uuid = '7102f854-c209-11ea-b7e0-932eff4dee21';
 
 
 INSERT INTO `cfg_app` (`uuid`, `nome`, `obs`, `inserted`, `updated`, `estabelecimento_id`, `user_inserted_id`, `user_updated_id`)
-VALUES ('7102f854-c209-11ea-b7e0-932eff4dee21','crosierapp-riotur','CrosierApp para o projeto Riotur',now(),now(),1,1,1);
+VALUES ('7102f854-c209-11ea-b7e0-932eff4dee21','crosierapp-iapo','CrosierApp Viação Iapó',now(),now(),1,1,1);
 
 
 COMMIT;
